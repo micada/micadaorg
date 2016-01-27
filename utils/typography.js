41bdec8c02b9918a7cf7a1e9c0c7e89a74abecc8
Mica A <mica.alaniz@gmail.com>
2016-01-26 22:52:35 -0600

    last try before removing typography altogether

--- utils/typography.js
+++ utils/typography.js
@@ -3,11 +3,15 @@ import Typography from 'typography';
 const options = {
   baseFontSize: '16px',
   baseLineHeight: '24px',
-  bodyFontFamily: '"Helvetica Neue", "Segoe UI", Helvetica, Arial, sans-serif',
-  headerFontFamily: '"Helvetica Neue", "Segoe UI", Helvetica, Arial, sans-serif',
+  bodyFontFamily: 'Verdana, Geneva, sans-serif',
+  headerFontFamily: 'Verdana, Geneva, sans-serif',
   bodyWeight: 300,
   headerWeight: 600,
   boldWeight: 600,
+  headerGray: 100,
+  headerGrayHue: 0,
+  bodyGray: 100,
+  bodyGrayHue: 0,
   modularScales: [
     {
       "scale": "minor third"
