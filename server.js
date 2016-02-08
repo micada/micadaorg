25bb100e25843429cd4a41f6a9ed2edabbf00347
Mica A <mica.alaniz@gmail.com>
2016-02-08 12:20:07 -0600

    put server back, made linter angry

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
-import {express} from 'express';
-import methodOverride from 'method-override';
-import path from 'path';
-import serveStatic from 'serve-static';
+var express = require('express');
+var methodOverride = require('method-override');
+var path = require('path');
+var serveStatic = require('serve-static')
 
 // web app middleware
-const app = express();
+var app = express();
 
 // bootstrap public/index.html
 app.use(methodOverride());
 
-app.use(serveStatic(__dirname + '/public'));
+// server-side router
+var router = express.Router();
 
-app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, 'public',
-    ['index.html', 'styles.css', 'favicon.ico', '05d72bd616722f39afef30004839ce08.eot',
-      '6ef18e62928bfbe78057f6604702062a.woff', 'd3b5827e4faf0b034d4b2bcaa6d43724.ttf']));
-});
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.use(serveStatic(__dirname + '/public'))
+app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, 'public', 
+      ['index.html', 'styles.css', 'favicon.ico', '05d72bd616722f39afef30004839ce08.eot', 
+      '6ef18e62928bfbe78057f6604702062a.woff', 'd3b5827e4faf0b034d4b2bcaa6d43724.ttf']))
+})
 
-let port = 80;
+var url = process.env.IP || '0.0.0.0'
+var port = 80;
+app.set('port', process.env.PORT || port)
 
-app.set('port', process.env.PORT || port);
+function listening() {
+    // NODE_ENV=production node index.js
+    if (process.env.NODE_ENV == 'production') {
+        console.log('Static server listening url %s on port %s', url, server.address().port);
+    }
+}
+
+var server = app.listen(port, listening);
