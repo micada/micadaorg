b5c799df25ec02f1e78fd0e332df3b2c0e8c9ab6
Mica Alaniz <malaniz@leapfrogonline.com>
2016-10-31 16:43:33 -0500

    adding tracking

--- html.jsx
+++ html.jsx
@@ -19,6 +19,16 @@ module.exports = React.createClass({
           <title>{title}</title>
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="stylesheet" type="text/css" href="/styles.css"/>
+          <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-15611717-1', 'auto');
+            ga('send', 'pageview');
+
+          </script>
         </head>
         <body>
           <div id="react-mount" dangerouslySetInnerHTML={{__html: this.props.body}} />
